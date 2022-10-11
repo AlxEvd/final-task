d67fdc5ceb9bba2173579576247e90e1e932b738
Alex <evdokimov.a.a@mail.ru>
2022-10-11 20:39:03 +0400

    программа готова

--- program/Program.cs
+++ program/Program.cs
@@ -1,24 +1,27 @@
 ﻿//Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-Console.WriteLine ($"сколько слов необходимо ввести? ");
+Console.WriteLine($"сколько слов ввести? ");
 int num = int.Parse(Console.ReadLine());
 
-string [] array = new string [num];
+string[] stringIn = new string[num];
+for (int i = 0; i < num; i++)
+{
+    stringIn[i] = Console.ReadLine();
+}
+
+string[] stringOut = new string[num];
+int j = 0;
 
 for (int i = 0; i < num; i++)
 {
-    Console.WriteLine ($"введите {i} слово ");
-    array[i] = int.Parse(Console.ReadLine());
-}
-
-int positive = 0;
-
-foreach (int item in array)
-{
-    if (item >= 0)
+    if (stringIn[i].Length <= 3)
     {
-      positive ++;
+        stringOut[j] = stringIn[i];
+        j++;
     }
-    
 }
-Console.WriteLine ($"положительных чисел {positive}");
+Console.WriteLine();
+for (int i = 0; i < num; i++)
+{
+    Console.WriteLine(stringOut[i]);
+}
