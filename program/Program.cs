fface9f1ce5cebf244240890fb1891fbe7b41dc3
Alex <evdokimov.a.a@mail.ru>
2022-10-11 20:29:28 +0400

    програма готова

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Console.WriteLine ($"сколько слов необходимо ввести? ");
+int num = int.Parse(Console.ReadLine());
+
+string [] array = new string [num];
+
+for (int i = 0; i < num; i++)
+{
+    Console.WriteLine ($"введите {i} слово ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+int positive = 0;
+
+foreach (int item in array)
+{
+    if (item >= 0)
+    {
+      positive ++;
+    }
+    
+}
+Console.WriteLine ($"положительных чисел {positive}");
